de4eee7c117f22897485bd299692a4a57757356b
joshua-db <104632410+joshua-db@users.noreply.github.com>
2022-08-04 16:32:49 +0200

    remove commented out commands

--- airlines_pipeline.sql
+++ airlines_pipeline.sql
@@ -4,14 +4,6 @@ SELECT * FROM cloud_files("/databricks-datasets/asa/airlines", "csv", map("heade
 
 -- COMMAND ----------
 
---SELECT * FROM bronze60101
-
--- COMMAND ----------
-
---SELECT _c8, COUNT(*) FROM bronze60101 GROUP BY _c8;
-
--- COMMAND ----------
-
 CREATE STREAMING LIVE TABLE silver_delays AS
 SELECT Year, Month, DayofMonth, UniqueCarrier, ArrDelay, DepDelay
 FROM STREAM(LIVE.bronze)
